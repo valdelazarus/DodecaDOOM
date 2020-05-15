5608985eab9fc543a35cdaa087048103b5bf3959
Ha Nguyen <valdelazarus@hotmail.com>
2020-05-14 21:45:19 -0400

    Implement player movement and combat 1 (animation, control)

---
+++ Assets/Player/Player Projectile/Scripts/PlayerProjectile.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    public float lifeTime;
+    public float speed;
+    public Vector3 direction;
+
+    Rigidbody rb;
+    Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        rb.velocity = direction * speed;
+    }
+
+    public void UpdateAnimation(float lastMoveX, float lastMoveY)
+    {
+        if (lastMoveX == -1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        if (lastMoveX == 1f)
+        {
+            direction = new Vector3(1f, 0f, 0f);
+        }
+        if (lastMoveX == -1f)
+        {
+            direction = new Vector3(-1f, 0f, 0f);
+        }
+        if (lastMoveY == 1f)
+        {
+            direction = new Vector3(0f, 0f, 1f);
+        }
+        if(lastMoveY == -1f)
+        {
+            direction = new Vector3(0f, 0f, -1f);
+        }
+
+        anim.SetFloat("lastMoveX", lastMoveX);
+        anim.SetFloat("lastMoveY", lastMoveY);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //deal damage to enemy
+        Destroy(gameObject);
+    }
+}
--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -6,32 +6,121 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float meleeAttackRate;//every ? seconds
+    public float rangedAttackRate; //every ? seconds
+    public GameObject projectile;
+    public Transform[] projectileSpawnPos;
 
     Rigidbody myRB;
-    SpriteRenderer spriteRenderer;
+    Animator myAnim;
+
+    bool canMelee = true, canRanged = true;
 
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        myAnim = GetComponent<Animator>();
     }
 
     
     void Update()
     {
         ProcessMovement();
-
+        ProcessAbility();
     }
 
     private void ProcessMovement()
     {
-        float vert = CrossPlatformInputManager.GetAxis("Vertical");
-        float horz = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        float horz = CrossPlatformInputManager.GetAxisRaw("Horizontal");
 
         myRB.velocity = new Vector3(
                 horz,
                 myRB.velocity.y,
                 vert
             ) * speed;
+
+        myAnim.SetFloat("moveX", myRB.velocity.x);
+        myAnim.SetFloat("moveY", myRB.velocity.z);
+
+        if (CrossPlatformInputManager.GetAxisRaw("Horizontal") >= 1 ||
+            CrossPlatformInputManager.GetAxisRaw("Horizontal") <= -1||
+            CrossPlatformInputManager.GetAxisRaw("Vertical") >= 1||
+            CrossPlatformInputManager.GetAxisRaw("Vertical") <= -1)
+        {
+            myAnim.SetFloat("lastMoveX", CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", CrossPlatformInputManager.GetAxisRaw("Vertical"));
+        }
+    }
+
+    void ProcessAbility()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire1") && canMelee)
+        {
+            canMelee = false;
+            myAnim.SetTrigger("melee");
+            StartCoroutine(ResetMelee());
+        }
+        else if (CrossPlatformInputManager.GetButtonDown("Fire2") && canRanged)
+        {
+            canRanged = false;
+            myAnim.SetTrigger("ranged");
+            StartCoroutine(ResetRanged());
+        }
+    }
+
+    IEnumerator ResetMelee()
+    {
+        yield return new WaitForSeconds(meleeAttackRate);
+        canMelee = true;
+    }
+
+    IEnumerator ResetRanged()
+    {
+        yield return new WaitForSeconds(rangedAttackRate);
+        canRanged = true;
+    }
+
+    string GetFacingDirection()
+    {
+        string direction = "down";
+        if (myAnim.GetFloat("lastMoveX") == 1f)
+        {
+            direction = "right";
+        }
+        if (myAnim.GetFloat("lastMoveX") == -1f)
+        {
+            direction = "left";
+        }
+        if (myAnim.GetFloat("lastMoveY") == 1f)
+        {
+            direction = "up";
+        }
+        if (myAnim.GetFloat("lastMoveY") == -1f)
+        {
+            direction = "down";
+        }
+        return direction;
+    }
+
+    public void SpawnProjectile()
+    {
+        string direction = GetFacingDirection();
+        GameObject spawned = null;
+        switch (direction)
+        {
+            case "down":
+                spawned = Instantiate(projectile, projectileSpawnPos[0].position, Quaternion.identity);                break;
+            case "up":
+                spawned = Instantiate(projectile, projectileSpawnPos[1].position, Quaternion.identity);
+                break;
+            case "left":
+                spawned = Instantiate(projectile, projectileSpawnPos[2].position, Quaternion.identity);
+                break;
+            case "right":
+                spawned = Instantiate(projectile, projectileSpawnPos[3].position, Quaternion.identity);     
+                break;
+        }
+        spawned?.GetComponent<PlayerProjectile>().UpdateAnimation(myAnim.GetFloat("lastMoveX"), myAnim.GetFloat("lastMoveY"));
     }
 }
